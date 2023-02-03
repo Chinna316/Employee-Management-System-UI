5fc9609845c868b196c3ff051e0ef56bcd5b4c58
chinna <chianna.annepu316@gmail.com>
2023-02-02 18:29:17 -0600

    1st page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AddEmployee from './Components/AddEmployee';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+   <>
+   <Navbar />
+   <AddEmployee />
+   </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-orange-700 h-16 font-bold pt-3'>
+        <h className="text-white mx-2 text-xl">Employee Management System</h>
+    </div>
+  )
+}
+
+export default Navbar

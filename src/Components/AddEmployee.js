de6d54205033b072a2a90c54925a508a9eefa6bb
chinna <chianna.annepu316@gmail.com>
2023-03-05 22:00:00 -0600

    commit

--- src/Components/AddEmployee.js
+++ src/Components/AddEmployee.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import EmployeeService from './Services/EmployeeService';
 
 const AddEmployee = () => {
--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -7,8 +7,8 @@ const Employee = ({employee, deleteEmployee}) => {
      <td className='text-left'> <div className='text-sm px-5'>{employee.lastName}</div> </td>
      <td className='text-left '> <div className='text-sm px-5'>{employee.email}</div> </td>
      <td className='text-right'>
-      <a href='#' className='px-5 text-blue-500 hover:text-blue-900 hover:cursor-pointer'> Edit </a>
-      <a onClick={(e, id) => deleteEmployee(e, employee.id)} className='px-5 text-blue-500 hover:text-blue-900 hover:cursor-pointer'>Delete</a>
+      <button className='px-5 text-blue-500 hover:text-blue-900 hover:cursor-pointer'> Edit </button>
+      <button onClick={(e, id) => deleteEmployee(e, employee.id)} className='px-5 text-blue-500 hover:text-blue-900 hover:cursor-pointer'>Delete</button>
      </td>
     </tr>
   )

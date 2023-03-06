c1fcc785865395fad3d49041888e31c4ac6c682d
chinna <chianna.annepu316@gmail.com>
2023-03-06 11:02:58 -0600

    coomit

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Employee = ({employee, deleteEmployee}) => {
+  const navigate = useNavigate();
+  const editEmployee = (e,id) => {
+    e.preventDefault();
+    navigate('/editEmployee/${id}')
+  }
   return (
     <tr key={employee.id}>
      <td className='text-left'> <div className='text-sm px-5'>{employee.firstName}</div> </td>
      <td className='text-left'> <div className='text-sm px-5'>{employee.lastName}</div> </td>
      <td className='text-left '> <div className='text-sm px-5'>{employee.email}</div> </td>
      <td className='text-right'>
-      <button className='px-5 text-blue-500 hover:text-blue-900 hover:cursor-pointer'> Edit </button>
+      <button onClick={(e,id) => editEmployee(e,id)} className='px-5 text-blue-500 hover:text-blue-900 hover:cursor-pointer'> Edit </button>
       <button onClick={(e, id) => deleteEmployee(e, employee.id)} className='px-5 text-blue-500 hover:text-blue-900 hover:cursor-pointer'>Delete</button>
      </td>
     </tr>
---
+++ src/Components/UpdateEmployee.js
@@ -0,0 +1,103 @@
+import React,{useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import EmployeeService from './Services/EmployeeService'
+
+const UpdateEmployee = () => {
+    const[id] = useParams;
+    const [employee, setemployee] = useState(    {
+        id:id,
+        firstName:"",
+        lastName:"",
+        email:"",
+    })
+
+    const handlechange = (e) =>{
+        const value = e.target.value;
+        setemployee({...employee,[e.target.name] : value})
+    }
+
+    const updateEmployee = (e) => {
+    e.preventDefault()
+    }
+
+    useEffect(() => {
+        const fetchData = async() => {
+        try {
+            const response = await EmployeeService.getEmployeeById(id);
+            setemployee(response.data)
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+    fetchData();
+    }, [])
+
+
+    const reset = (e) => {
+        e.preventDefault();
+        setemployee({ 
+        id:"",
+        firstName:"",
+        lastName:"",
+        email:"",})
+    }
+    
+    
+
+  return (
+    <div className="flex max-w-2xl mx-auto shadow border-b-2">
+    <div className="px-8 py-8">
+    <div className='font-thin text-2xl tracking-wider'>
+      <h1>Update Employee</h1>
+    </div>
+   
+
+
+<div className='items-center justify-center h-14 w-full my-4'>
+    <label className='block text-gray-600 text-sm font-normal'>
+        FirstName
+    </label>
+    <input 
+       type="text"
+       name='firstName'
+       value={employee.firstName}
+       onChange={(e) => handlechange(e)}
+       className='h-10 w-96 border mt-2 px-2 py-2'></input>
+</div>
+
+<div className='items-center justify-center h-14 w-full my-4'>
+    <label className='block text-gray-600 text-sm font-normal'>
+        LastName
+    </label>
+    <input 
+       type="text"
+       name='lastName'
+       value={employee.lastName}
+       onChange={(e) => handlechange(e)}
+       className='h-10 w-96 border mt-2 px-2 py-2'></input>
+</div>
+
+<div className='items-center justify-center h-14 w-full my-4'>
+    <label className='block text-gray-600 text-sm font-normal'>
+        Email
+    </label>
+    <input 
+       type="email"
+       name='email'
+       value={employee.email}
+       onChange={(e) => handlechange(e)}
+       className='h-10 w-96 border mt-2 px-2 py-2'></input>
+</div>
+
+<div className='items-center justify-center h-14 w-full my-4 space-x-5 text-white'>
+    <button onClick= {updateEmployee}className='bg-green-700 rounded hover:bg-green-400 px-2 mt-2 py-1'>Update</button>
+    <button onClick={reset}className='bg-red-700 rounded hover:bg-red-400 px-2 mt-2 py-1'>Cancel</button>
+</div>
+
+</div>
+</div>
+  )
+}
+
+export default UpdateEmployee
